edaeba6a541006544adb1d2d551761f69725ad03
KgitWH21 <kwhayden11@gmail.com>
2026-07-16 09:32:30 -0700

    random word function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 import { useState } from 'react'
+import words from './words.json'
+
+//helper functions are best outside of the main App
+function getRandomWord() {
+  const randomIndex = Math.floor(Math.random() * words.length);
+  return words[randomIndex].toLowerCase()
+} 
 
 function App() {
+  const [puzzle, setPuzzle] = useState("");
+  const [guessedLetters, setGuess] = useState([]);
+
+
   return (
     <div>App</div>
   )
